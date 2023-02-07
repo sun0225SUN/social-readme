5accbb618f382f54bc136ddc22b27469827bace4
小孙同学 <sun412926@gmail.com>
2023-02-07 09:53:41 +0800

    Update social.py

--- social.py
+++ social.py
@@ -76,7 +76,7 @@ def format_time(timestamp) -> datetime:
 
 def generate_rating_star(desc) -> str:
     pattern = re.compile(r'<p>推荐: [\s\S]+</p>')
-    matches = re.findall(pattern, desc)
+    matches = re.search(pattern, desc)
     if len(matches) > 0:
         return DOUBAN_RATING[matches[0]]
     return ''
